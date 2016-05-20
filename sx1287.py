77b1a98a44801959eea9d2daada18cb410ae6ac8
filmcup <filmcup86@gmail.com>
2016-05-20 20:43:47 +0700

    Update sx1287.py

--- sx1287.py
+++ sx1287.py
@@ -21,6 +21,15 @@ import time
 
 from sx1278_function import *
 
+"""*********************************************************
+**Parameter table define**
+sx1276_7_8FreqTbl = [[0x6C, 0x80, 0x00]] #433MHz
+sx1276_7_8PowerTbl = [0xFF,0xFC,0xF9,0xF6]  #20 17 14 11 dbm
+sx1276_7_8SpreadFactorTbl = [6,7,8,9,10,11,12]
+sx1276_7_8LoRaBwTbl = [0,1,2,3,4,5,6,7,8,9] 
+#7.8KHz,10.4KHz,15.6KHz,20.8KHz,31.2KHz,41.7KHz,62.5KHz,125KHz,250KHz,500KHz
+*********************************************************"""
+
 # Config
 mode = 0x01 #lora mode
 Freq_Sel = 0x00 #433M
@@ -49,6 +58,7 @@ while(1):
 	#Master
 	#i = i + 1
 	#sx1276_7_8Data[0] = 'A'
+	"""
 	GPIO.output(led, GPIO.HIGH) # turn the LED on (GPIO.HIGH is the voltage level)
 	sx1276_7_8_LoRaEntryTx()
 	sx1276_7_8_LoRaTxPacket("AO",2)
@@ -56,14 +66,17 @@ while(1):
 	sx1276_7_8_LoRaEntryRx()
 	print "send"
 	time.sleep(2)
+	"""
 
-	""" 
-	if(sx1276_7_8_LoRaRxPacket())
+	temp = sx1276_7_8_LoRaRxPacket(); 
+	if(sx1276_7_8_LoRaRxPacket() != 0)
 		GPIO.output(led, GPIO.HIGH); # turn the LED on (GPIO.HIGH is the voltage level)
 		delay(500);
 		GPIO.output(led, GPIO.LOW); # turn the LED on (GPIO.HIGH is the voltage level)
 		delay(500);
-	"""
+		sx1276_7_8_LoRaEntryRx()
+		print "Data: " + temp
+	
 	#slaver
 	"""	
 	if(sx1276_7_8_LoRaRxPacket())
