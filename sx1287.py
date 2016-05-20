7db8e8be673871a068824f7bc7c492485e79a9d3
filmcup <filmcup86@gmail.com>
2016-05-20 18:00:20 +0700

    Update sx1287.py

--- sx1287.py
+++ sx1287.py
@@ -20,15 +20,6 @@ import RPi.GPIO as GPIO
 import time
 
 from sx1278_function import *
-from sx1278_registers import *
-
-# put your main code here, to run repeatedly:
-mode = 0x01 #lora mode
-Freq_Sel = 0x00 #433M
-Power_Sel = 0x00 #
-Lora_Rate_Sel = 0x06 #
-BandWide_Sel = 0x07
-Fsk_Rate_Sel = 0x00
 
 print "reset"
 reset_sx1276()
