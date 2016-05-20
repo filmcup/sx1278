d33eb7a5eec7b9290f22f61b191ccd311d63debb
filmcup <filmcup86@gmail.com>
2016-05-20 17:46:45 +0700

    Update sx1278_function.py

--- sx1278_function.py
+++ sx1278_function.py
@@ -179,62 +179,6 @@ def BurstWrite2(adr, ptr, leng):
 
         return
 
-"""***********************************************
-# RF module: sx1276_7_8
-# FSK:
-# Carry Frequency: 434MHz
-# Bit Rate: 1.2Kbps/2.4Kbps/4.8Kbps/9.6Kbps
-# Tx Power Output: 20dbm/17dbm/14dbm/11dbm
-# Frequency Deviation: +/-35KHz
-# Receive Bandwidth: 83KHz
-# Coding: NRZ
-# Packet Format: 0x5555555555+0xAA2DD4+"Mark1 Lora sx1276_7_8" (total: 29bytes)
-# LoRa:
-# Carry Frequency: 434MHz
-# Spreading Factor: 6/7/8/9/10/11/12
-# Tx Power Output: 20dbm/17dbm/14dbm/11dbm
-# Receive Bandwidth:7.8KHz/10.4KHz/15.6KHz/20.8KHz/31.2KHz/41.7KHz/62.5KHz/125KHz/250KHz/500KHz
-# Coding: NRZ
-# Packet Format: "HR_WT Lora sx1276_7_8" (total: 21 bytes)
-# Tx Current: about 120mA (RFOP=+20dBm,typ.)
-# Rx Current: about 11.5mA (typ.)
-*********************************************************"""
-########################/ LoRa mode
-#########################
-#Error Coding rate (CR)setting
-CR_4_5 = 0
-CR_4_6 = 1
-CR_4_7 = 1
-CR_4_8 = 1
-
-if CR_4_5 == 0:
-	CR = 0x01
-elif CR_4_6 == 0:
-	CR = 0x02
-elif CR_4_7 == 0:
-	CR = 0x03
-elif CR_4_8 == 0:
-	CR = 0x04
-
-
-#CRC Enable
-CRC_EN = 1
-if CRC_EN == 1:
-	CRC = 0x01 #CRC Enable
-else:
-	CRC = 0x00
-
-"""*********************************************************
-**Parameter table define
-*********************************************************"""
-sx1276_7_8FreqTbl = [[0x6C, 0x80, 0x00]] #433MHz
-sx1276_7_8PowerTbl = [0xFF,0xFC,0xF9,0xF6]  #20 17 14 11 dbm
-sx1276_7_8SpreadFactorTbl = [6,7,8,9,10,11,12]
-sx1276_7_8LoRaBwTbl = [0,1,2,3,4,5,6,7,8,9] 
-#7.8KHz,10.4KHz,15.6KHz,20.8KHz,31.2KHz,41.7KHz,62.5KHz,125KHz,250KHz,500KHz
-
-sx1276_7_8Data = "Xark1 Lora sx1276_7_8"
-RxData = None
 """*********************************************************
 **Name: sx1276_7_8_Standby
 **Function: Entry standby mode
